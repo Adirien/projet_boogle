d2c132a5213c965c465278347ff4615f37bbdc6a
ulgor <ulgor@free.fr>
2024-12-09 23:06:25 +0100

    fin de journee. Modif dicolangue, tour

--- boogle/Tour.cs
+++ boogle/Tour.cs
@@ -27,21 +27,30 @@ public class Tour
         while(true)
         {
            
-            string nouveau_mot = Console.ReadLine();
-            if (!sablier.TempsSEcoule())
-            {
-                // la saisie du mot se fait apres le temps autorisé.
-                //on ne prend pas en compte le mot
-                break;
+            string? mot = Console.ReadLine();
+            if (mot.Length>0){
+                mot = mot.ToUpper();
+                if (!sablier.TempsSEcoule())
+                {
+                    // la saisie du mot se fait apres le temps autorisé.
+                    //on ne prend pas en compte le mot
+                    break;
+                }
+                else
+                {
+                    // A FAIRE ici:
+                    //verifier que le mot existe sur plateau
+                    if (this.plateau.Contain(mot) && this.joueur.Contains(mot)){
+                        // Il faut le dictionnaire pour verifier qu'il existe 
+                        // puis on clcule le nombre points du mots et on met a jour le score du joueur
+                    }
+                    // ajouter le mot a la liste du joueur,
+                    
+                    //verifier que mot existe dans dico
+                    //calculer les points
+                }
             }
-            else
-            {
-                // A FAIRE ici:
-                // ajouter le mot a la liste du joueur,
-                //verifier que le mot existe sur plateau
-                //verifier que mot existe dans dico
-                //calculer les points
-            }
+            
         }
         Console.WriteLine("Le tour pour le {0} est fini", this.joueur.Pseudo);
     }
